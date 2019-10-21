c5ed1f071e9521d2117e70238b2f8f4bf143fda6
sungwoo1548 <swo1548@gmail.com>
2019-10-21 21:27:20 +0900

    Updates

---
+++ precache-manifest.e1219de796516208ff53fa535c879ba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52aaf4385afceb572f455db93278f05a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c0ae7dd851d7f3cbefde",
+    "url": "/portfolio/static/css/main.92066db2.chunk.css"
+  },
+  {
+    "revision": "a06bf1eb57594464c5b1",
+    "url": "/portfolio/static/js/2.2ff8c103.chunk.js"
+  },
+  {
+    "revision": "c0ae7dd851d7f3cbefde",
+    "url": "/portfolio/static/js/main.0b479bc5.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
